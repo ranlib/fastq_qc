300a3796a8508d2c906f425963d5c3496a12c4ea
Dieter Best <dieterbest@gmail.com>
2023-07-20 15:09:26 -0700

    generate_contamination_metadata.sh: change name of output contaminants multi-fasta file

--- generate_contamination_metadata.sh
+++ generate_contamination_metadata.sh
@@ -8,7 +8,7 @@
 (zcat adapters.fa.gz | awk -F\> 'BEGIN{OFS="\t"; s=0}/^>/{s++; print "contam_group"s,1,$2}'; printf "reference\t0\tNC_000962.3") > clockwork_decontamination_metadata.tsv
 
 # contaminants multi-fasta
-(zcat adapters.fa.gz ; cat ~/Analysis/varpipe4/references/NC_000962.3/NC_000962.3.fasta) | gzip > contaminants.fa.gz
+(zcat adapters.fa.gz ; cat ~/Analysis/varpipe4/references/NC_000962.3/NC_000962.3.fasta) | gzip > clockwork_contaminants.fa.gz
 
 exit 0
 
